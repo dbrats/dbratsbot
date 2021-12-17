3ee4d3876ec849b2f4d2cf137e0739c410c25ded
Daniel Bratsberg <daniel.bratsberg@finn.no>
2021-12-17 20:55:22 +0100

    Removing totalcount

--- findnamescog.py
+++ findnamescog.py
@@ -40,7 +40,6 @@ friendYoutubeMap = {
     "Jakob" : "https://www.youtube.com/watch?v=EBqCSseQXsI"
 }
 
-totalCount = 0
 chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 def saveName(name, url):
@@ -104,6 +103,5 @@ async def checkNextBatch(client):
                         count = count + 1
                         if count % 100 == 0:
                             setLastChecked(a, b, c, d, e)
-                            totalCount = totalCount + count
-                            print ('Count: ' + str(count) + ' : ' + a + b + c + d + e + " : Tot: " + totalCount)
-                            return           
+                            print ('Count: ' + str(count) + ' : ' + a + b + c + d + e)
+                            return
